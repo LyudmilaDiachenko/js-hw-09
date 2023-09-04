04e7430df4f9beee12f2990e100f0b33518c4e35
LyudmilaDiachenko <lyudmila.diachenko@gmail.com>
2023-09-04 15:52:25 +0300

    add task

---
+++ task-1_4.js
@@ -0,0 +1,12 @@
+// #4
+// Задача: Фільтрація та групування книг
+// У нас є список книг. Відфільтруйте цей список за певною категорією Fiction, а потім
+// групувати книги за авторами. При цьому виведіть лише імена авторів та список їхніх книг.
+// Список книг
+// const books = [
+//  { title: 'Book 1', author: 'Author A', category: 'Fiction' },
+//  { title: 'Book 2', author: 'Author B', category: 'Non-Fiction' },
+//  { title: 'Book 3', author: 'Author A', category: 'Fiction' },
+//  { title: 'Book 4', author: 'Author C', category: 'Mystery' },
+//  { title: 'Book 5', author: 'Author B', category: 'Fiction' },
+// ];
